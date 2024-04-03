5651829b3bc6345d02e29e73852c1c668b76c9b5
Linus Dinesjö <linus.dinesjo@gmail.com>
2024-04-03 20:02:37 +0200

    fix #9

--- src/routes/Index/IndexLayout.tsx
+++ src/routes/Index/IndexLayout.tsx
@@ -12,9 +12,10 @@ import {
   Fab,
   Grow,
   Divider,
+  Link,
 } from "@mui/material";
 import Cover from "../../components/Cover";
-import { Form, redirect, useLoaderData, useNavigation } from "react-router-dom";
+import { Form, redirect, useLoaderData, useNavigation, Link as RouterLink } from "react-router-dom";
 import { Habit, fetchHabits, registerHabitsToday } from "../../habitsModel";
 import { Check, CheckBox, CheckBoxOutlineBlank, DoneAll } from "@mui/icons-material";
 import { IconMap } from "../../utils/IconMap";
@@ -52,83 +53,97 @@ export default function IndexLayout() {
         </Typography>
         <Typography variant="subtitle1">Have you kept up with your habits lately?</Typography>
         <Divider sx={{ my: 1 }} />
-        <Typography variant="subtitle2" color="text.secondary">
-          Register today's habits below:
-        </Typography>
-        <List
-          sx={{
-            minWidth: 260,
-            maxHeight: "calc(100vh - 200px)",
-            overflow: "auto",
-          }}
-        >
-          {Object.keys(habits).map((key) => {
-            const habit = habits[key];
-            const isChecked = checked.includes(key);
-            return (
-              <Box key={key}>
-                <ListItem disablePadding>
-                  <ListItemButton
-                    sx={{ p: 1 }}
-                    onClick={() => setChecked((prev) => (isChecked ? prev.filter((id) => id !== key) : [...prev, key]))}
-                  >
-                    <ListItemAvatar sx={{ color: habit.color }}>
-                      <Avatar
-                        sx={{
-                          bgcolor: habit.color || "text.primary",
-                        }}
+        {habits ? (
+          <>
+            <Typography variant="subtitle2" color="text.secondary">
+              Register today's habits below:
+            </Typography>
+            <List
+              sx={{
+                minWidth: 260,
+                maxHeight: "calc(100vh - 200px)",
+                overflow: "auto",
+              }}
+            >
+              {Object.keys(habits).map((key) => {
+                const habit = habits[key];
+                const isChecked = checked.includes(key);
+                return (
+                  <Box key={key}>
+                    <ListItem disablePadding>
+                      <ListItemButton
+                        sx={{ p: 1 }}
+                        onClick={() =>
+                          setChecked((prev) => (isChecked ? prev.filter((id) => id !== key) : [...prev, key]))
+                        }
                       >
-                        {IconMap[habit.icon || "default"]}
-                      </Avatar>
-                    </ListItemAvatar>
-                    <ListItemText
-                      primary={<Typography sx={{ color: habit.color }}>{habit.name}</Typography>}
-                      secondary={toFriendlyFrequency(habit)}
+                        <ListItemAvatar sx={{ color: habit.color }}>
+                          <Avatar
+                            sx={{
+                              bgcolor: habit.color || "text.primary",
+                            }}
+                          >
+                            {IconMap[habit.icon || "default"]}
+                          </Avatar>
+                        </ListItemAvatar>
+                        <ListItemText
+                          primary={<Typography sx={{ color: habit.color }}>{habit.name}</Typography>}
+                          secondary={toFriendlyFrequency(habit)}
+                        />
+                        {isChecked && <input type="hidden" name="habitIds" value={key} />}
+                        {<Checkbox icon={<CheckBoxOutlineBlank />} checkedIcon={<CheckBox />} checked={isChecked} />}
+                      </ListItemButton>
+                    </ListItem>
+                    <LinearProgress
+                      variant="buffer"
+                      valueBuffer={getProgressBuffer(habit)}
+                      value={getProgress(habit)}
+                      sx={{
+                        ".MuiLinearProgress-bar1Buffer": {
+                          backgroundColor: habit.color,
+                        },
+                        ".MuiLinearProgress-dashed": {
+                          display: "none",
+                        },
+                      }}
                     />
-                    {isChecked && <input type="hidden" name="habitIds" value={key} />}
-                    {<Checkbox icon={<CheckBoxOutlineBlank />} checkedIcon={<CheckBox />} checked={isChecked} />}
-                  </ListItemButton>
-                </ListItem>
-                <LinearProgress
-                  variant="buffer"
-                  valueBuffer={getProgressBuffer(habit)}
-                  value={getProgress(habit)}
-                  sx={{
-                    ".MuiLinearProgress-bar1Buffer": {
-                      backgroundColor: habit.color,
-                    },
-                    ".MuiLinearProgress-dashed": {
-                      display: "none",
-                    },
-                  }}
-                />
-              </Box>
-            );
-          })}
-        </List>
-        <Box
-          sx={{
-            position: "absolute",
-            width: "100%",
-            display: "flex",
-            justifyContent: "center",
-            bottom: "-2rem",
-          }}
-        >
-          {navigation.state === "submitting" ? (
-            <Fab variant="extended" color="primary" type="submit" disabled>
-              <Check sx={{ mr: 1 }} />
-              Registering...
-            </Fab>
-          ) : (
-            <Grow in={checked.length > 0}>
-              <Fab variant="extended" color="primary" type="submit" disabled={!checked.length}>
-                {checked.length === 1 ? <Check sx={{ mr: 1 }} /> : <DoneAll sx={{ mr: 1 }} />}
-                Register
-              </Fab>
-            </Grow>
-          )}
-        </Box>
+                  </Box>
+                );
+              })}
+            </List>
+            <Box
+              sx={{
+                position: "absolute",
+                width: "100%",
+                display: "flex",
+                justifyContent: "center",
+                bottom: "-2rem",
+              }}
+            >
+              {navigation.state === "submitting" ? (
+                <Fab variant="extended" color="primary" type="submit" disabled>
+                  <Check sx={{ mr: 1 }} />
+                  Registering...
+                </Fab>
+              ) : (
+                <Grow in={checked.length > 0}>
+                  <Fab variant="extended" color="primary" type="submit" disabled={!checked.length}>
+                    {checked.length === 1 ? <Check sx={{ mr: 1 }} /> : <DoneAll sx={{ mr: 1 }} />}
+                    Register
+                  </Fab>
+                </Grow>
+              )}
+            </Box>
+          </>
+        ) : (
+          <Typography variant="body2" align="center">
+            No habits found.{" "}
+            <Link component={RouterLink} to="/my-habits">
+              Go
+            </Link>{" "}
+            create one!
+          </Typography>
+        )}
       </Box>
     </Cover>
   );
--- src/routes/MyHabits/MyHabitsIndex.tsx
+++ src/routes/MyHabits/MyHabitsIndex.tsx
@@ -4,6 +4,7 @@ import {
   CircularProgress,
   Divider,
   Fab,
+  Link,
   List,
   ListItem,
   ListItemAvatar,
@@ -31,53 +32,53 @@ export default function MyHabitsIndex() {
   const navigation = useNavigation();
 
   return (
-    <>
-      <Cover>
-        <Box sx={{ position: "relative" }}>
-          <Typography variant="h5" align="center" gutterBottom>
-            My Habits
-          </Typography>
-          <Divider />
-          <List
-            sx={{
-              minWidth: 260,
-              maxHeight: "calc(100vh - 200px)",
-              overflow: "auto",
-            }}
-          >
-            {Object.keys(habits).map((key) => {
-              const habit = habits[key];
-              return (
-                <ListItem disablePadding key={key}>
-                  <ListItemButton sx={{ p: 1 }} onClick={() => navigate(`/my-habits/${key}`)}>
-                    <ListItemAvatar sx={{ color: habit.color }}>
-                      <Avatar
-                        sx={{
-                          bgcolor: habit.color || "text.primary",
-                        }}
-                      >
-                        {IconMap[habit.icon || "default"]}
-                      </Avatar>
-                    </ListItemAvatar>
-                    <ListItemText
-                      primary={<Typography sx={{ color: habit.color }}>{habit.name}</Typography>}
-                      secondary={habit.description}
-                    />
-                  </ListItemButton>
-                </ListItem>
-              );
-            })}
-          </List>
-          <Box
-            sx={{
-              position: "absolute",
-              width: "100%",
-              display: "flex",
-              justifyContent: "center",
-              bottom: "-2rem",
-            }}
-          >
-            <Form action="/my-habits/new-habit" method="post">
+    <Cover>
+      <Box component={Form} action="/my-habits/new-habit" method="post" sx={{ position: "relative" }}>
+        <Typography variant="h5" align="center" gutterBottom>
+          My Habits
+        </Typography>
+        <Divider sx={{ my: 1 }} />
+        {habits ? (
+          <>
+            <List
+              sx={{
+                minWidth: 260,
+                maxHeight: "calc(100vh - 200px)",
+                overflow: "auto",
+              }}
+            >
+              {Object.keys(habits).map((key) => {
+                const habit = habits[key];
+                return (
+                  <ListItem disablePadding key={key}>
+                    <ListItemButton sx={{ p: 1 }} onClick={() => navigate(`/my-habits/${key}`)}>
+                      <ListItemAvatar sx={{ color: habit.color }}>
+                        <Avatar
+                          sx={{
+                            bgcolor: habit.color || "text.primary",
+                          }}
+                        >
+                          {IconMap[habit.icon || "default"]}
+                        </Avatar>
+                      </ListItemAvatar>
+                      <ListItemText
+                        primary={<Typography sx={{ color: habit.color }}>{habit.name}</Typography>}
+                        secondary={habit.description}
+                      />
+                    </ListItemButton>
+                  </ListItem>
+                );
+              })}
+            </List>
+            <Box
+              sx={{
+                position: "absolute",
+                width: "100%",
+                display: "flex",
+                justifyContent: "center",
+                bottom: "-2rem",
+              }}
+            >
               {navigation.state === "submitting" ? (
                 <Fab type="submit" variant="extended" color="primary" disabled>
                   <CircularProgress size="2rem" sx={{ mr: 1 }} />
@@ -89,10 +90,18 @@ export default function MyHabitsIndex() {
                   New Habit
                 </Fab>
               )}
-            </Form>
-          </Box>
-        </Box>
-      </Cover>
-    </>
+            </Box>
+          </>
+        ) : (
+          <Typography variant="body2" align="center">
+            No habits found. Create one{" "}
+            <Link component="button" type="submit">
+              here
+            </Link>
+            .
+          </Typography>
+        )}
+      </Box>
+    </Cover>
   );
 }
