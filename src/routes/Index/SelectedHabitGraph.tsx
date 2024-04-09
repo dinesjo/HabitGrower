d6ac78b51a83f1af5e5d39bfb0043e4f6afdce5e
Linus Dinesjö <linus.dinesjo@gmail.com>
2024-04-09 12:04:55 +0200

    add missing data points
    Fixes tooltip bug

--- src/routes/Index/SelectedHabitGraph.tsx
+++ src/routes/Index/SelectedHabitGraph.tsx
@@ -19,18 +19,30 @@ export default function SelectedHabitGraph({ habit }: { habit: Habit }) {
     return acc;
   }, []);
 
+  // Fill in missing dates with value 0
+  const firstDay = dateData[0]; // e.g. 19815
+  const lastDay = dateData[dateData.length - 1]; // e.g. 19825
+  const allDays = Array.from({ length: lastDay.date - firstDay.date + 1 }, (_, i) => firstDay.date + i);
+  const missingDays = allDays.filter((day) => !dateData.find((data) => data.date === day));
+  missingDays.forEach((date) => dateData.push({ date, value: 0 }));
+  dateData.sort((a, b) => a.date - b.date);
+
+  // Get max value in the dataSet
+  const maxValue = Math.max(...dateData.map((data) => data.value));
+
   return habit.dates ? (
     <>
       <Typography variant="body2" color="text.secondary">
         You've registered this habit {habit.dates.length} times.
       </Typography>
       <LineChart
+        dataset={dateData}
         width={300}
         height={200}
         xAxis={[
           {
             dataKey: "date",
-            scaleType: "time",
+            scaleType: "point",
             valueFormatter: (unix) => dayjs.unix(unix * (60 * 60 * 24)).format("MMM D"),
             label: "Date",
           },
@@ -38,14 +50,13 @@ export default function SelectedHabitGraph({ habit }: { habit: Habit }) {
         yAxis={[
           {
             dataKey: "value",
-            scaleType: "linear",
             min: 0,
+            max: Math.max(habit.frequency || 0, maxValue),
             tickMinStep: 1, // integers
             label: "Habit done",
           },
         ]}
         series={[{ dataKey: "value", label: habit.name, color: habit.color || "#90c65b" }]}
-        dataset={dateData}
       />
     </>
   ) : (
