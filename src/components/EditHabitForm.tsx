42742b455414f7c60b01df5b2277e806e51c3d59
Linus Dinesjö <dinesjo@kth.se>
2024-05-16 12:19:20 +0200

    style: improve edit-form UI

--- src/components/EditHabitForm.tsx
+++ src/components/EditHabitForm.tsx
@@ -4,20 +4,21 @@ import {
   Button,
   Card,
   CardActions,
-  Container,
+  CardContent,
+  Chip,
+  Divider,
   FormControl,
   Grid,
   InputLabel,
   MenuItem,
   Select,
-  Stack,
   TextField,
   Typography,
 } from "@mui/material";
 import { IconMap } from "../utils/IconMap";
 import { toFriendlyString } from "../utils/helpers.tsx";
 import DeleteHabitWithConfirm from "./DeleteHabitWithConfirm.tsx";
-import { Save } from "@mui/icons-material";
+import { ColorLens, EventRepeat, Save } from "@mui/icons-material";
 import BackButton from "./BackButton.tsx";
 
 async function loader({ params }: LoaderFunctionArgs<{ id: string }>) {
@@ -61,91 +62,106 @@ export default function EditHabitForm() {
       <CardActions>
         <BackButton />
       </CardActions>
-      <Container maxWidth="xs" component={Form} autoComplete="off" method="post">
+      <Form autoComplete="off" method="post">
         <Typography variant="h5" align="center" gutterBottom>
           Edit Habit: <b>{habit.name}</b>
         </Typography>
-        <Grid container spacing={2} sx={{ py: 1 }}>
-          <Grid item xs={12} sm={6}>
-            <TextField required name="name" label="Habit Name" defaultValue={habit.name} fullWidth />
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <TextField
-              name="description"
-              label="Description"
-              defaultValue={habit.description || ""}
-              multiline
-              fullWidth
-            />
-          </Grid>
-          <Grid item xs={4}>
-            <FormControl required fullWidth>
-              <InputLabel>Icon</InputLabel>
-              <Select autoWidth name="icon" label="Icon" defaultValue={habit.icon || ""}>
-                {Object.keys(IconMap).map((icon) => (
-                  <MenuItem key={icon} value={icon}>
-                    {IconMap[icon]}
-                  </MenuItem>
-                ))}
-              </Select>
-            </FormControl>
-          </Grid>
-          <Grid item xs={8}>
-            <FormControl fullWidth>
-              <InputLabel>Color</InputLabel>
-              <Select
-                name="color"
-                label="Color"
-                defaultValue={habit.color || ""}
-                renderValue={(value) => {
-                  return <Typography sx={{ color: value }}>{toFriendlyString(value)}</Typography>;
-                }}
+        <CardContent>
+          <Grid container spacing={2}>
+            {/* Section tile */}
+            <Grid item xs={12}>
+              <Divider>
+                <Chip icon={<ColorLens />} label="APPEARANCE" size="small" />
+              </Divider>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField required name="name" label="Habit Name" defaultValue={habit.name} fullWidth />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                name="description"
+                label="Description"
+                defaultValue={habit.description || ""}
+                multiline
                 fullWidth
-              >
-                <MenuItem value="">None</MenuItem>
-                {colorChoices.map((color) => (
-                  <MenuItem key={color} value={color} sx={{ color: color }}>
-                    {toFriendlyString(color)}
-                  </MenuItem>
-                ))}
-              </Select>
-            </FormControl>
-          </Grid>
-          <Grid item xs={5}>
-            <TextField
-              name="frequency"
-              label="How often?"
-              type="number"
-              defaultValue={habit.frequency || ""}
-              fullWidth
-            />
-          </Grid>
-          <Grid item xs={7}>
-            <FormControl fullWidth>
-              <InputLabel id="frequencyUnit">Every</InputLabel>
-              <Select
-                name="frequencyUnit"
-                label="Every"
-                labelId="frequencyUnit"
-                defaultValue={habit.frequencyUnit || ""}
+              />
+            </Grid>
+            <Grid item xs={4}>
+              <FormControl required fullWidth>
+                <InputLabel>Icon</InputLabel>
+                <Select autoWidth name="icon" label="Icon" defaultValue={habit.icon || ""}>
+                  {Object.keys(IconMap).map((icon) => (
+                    <MenuItem key={icon} value={icon}>
+                      {IconMap[icon]}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </Grid>
+            <Grid item xs={8}>
+              <FormControl fullWidth>
+                <InputLabel>Color</InputLabel>
+                <Select
+                  name="color"
+                  label="Color"
+                  defaultValue={habit.color || ""}
+                  renderValue={(value) => {
+                    return <Typography sx={{ color: value }}>{toFriendlyString(value)}</Typography>;
+                  }}
+                  fullWidth
+                >
+                  <MenuItem value="">None</MenuItem>
+                  {colorChoices.map((color) => (
+                    <MenuItem key={color} value={color} sx={{ color: color }}>
+                      {toFriendlyString(color)}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </Grid>
+            {/* Section tile */}
+            <Grid item xs={12}>
+              <Divider>
+                <Chip icon={<EventRepeat />} label="HOW OFTEN?" size="small" />
+              </Divider>
+            </Grid>
+            <Grid item xs={5}>
+              <TextField
+                name="frequency"
+                placeholder="3"
+                type="number"
+                defaultValue={habit.frequency || ""}
                 fullWidth
-              >
-                <MenuItem value="">None</MenuItem>
-                <MenuItem value="day">Day</MenuItem>
-                <MenuItem value="week">Week</MenuItem>
-                <MenuItem value="month">Month</MenuItem>
-              </Select>
-            </FormControl>
+              />
+            </Grid>
+            <Grid item xs={7}>
+              <FormControl fullWidth>
+                <InputLabel id="frequencyUnit">Every</InputLabel>
+                <Select
+                  name="frequencyUnit"
+                  label="Every"
+                  labelId="frequencyUnit"
+                  defaultValue={habit.frequencyUnit || ""}
+                  fullWidth
+                >
+                  <MenuItem value="">None</MenuItem>
+                  <MenuItem value="day">Day</MenuItem>
+                  <MenuItem value="week">Week</MenuItem>
+                  <MenuItem value="month">Month</MenuItem>
+                </Select>
+              </FormControl>
+            </Grid>
           </Grid>
-        </Grid>
-        <Stack direction={"row"} spacing={2} my={1}>
+        </CardContent>
+        <Divider />
+        <CardActions>
           <Button startIcon={<Save />} variant="contained" type="submit" disabled={navigation.state === "submitting"}>
             Save
           </Button>
           <Button onClick={handleBack}>Cancel</Button>
           <DeleteHabitWithConfirm habit={habit} id={id!} />
-        </Stack>
-      </Container>
+        </CardActions>
+      </Form>
     </Card>
   );
 }
